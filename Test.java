5fe3686ba18129474ec11fbba0a94f94b6ad5fef
Jagbir-P <purej1485@wrdsb.ca>
2023-03-08 18:32:59 -0500

    Possibly have Add working

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+
+
+
+        LinkedList list = new LinkedList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.printOut();
+    }
+}
